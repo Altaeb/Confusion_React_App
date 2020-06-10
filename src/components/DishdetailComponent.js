a866ea4efad95142a8a193e778abfc77ea60c7ca
Altaeb <abd2010ly@gmail.com>
2020-06-10 21:35:51 +0200

    Refactoring dishDetailComponent.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,48 +1,59 @@
-import React, { Component } from 'react'
+import React, { Component, isValidElement } from 'react'
 import { Card, CardImg, CardText, CardBody, CardTitle,CardColumns } from 'reactstrap';
 import Moment from 'moment';
 
 class DishDetail extends Component{
-    constructor(props) {
-        super(props)
-        this.state = {}
+
+
+    renderDish(dish) {
+            return(
+              <div className="col-12 col-md-5 m-1">
+                <Card>
+                  <CardImg top src={dish.image} alt={dish.name}/>
+                  <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                  </CardBody>
+                </Card>
+              </div>
+            );
     }
+
+    renderComments(comments) {
+      if (comments != null)
+          return(
+            <div className="col-12 col-md-5 m-1">
+              <h4>Comments</h4>
+              <ul className="list-unstyled">
+                  {comments.map((comment) => {
+                      return (
+                        <li key={comment.id}>
+                          <p>{comment.comment}</p>
+                      <p>-- {comment.author} , {Moment(comment.date).format('MMM d, yyyy')}</p>
+                        </li>
+                      );
+                  })}
+              </ul>
+            </div>
+          );
+    }
+
     render() {
         const dish = this.props.dish;
         if (dish != null) {
-            const comments = dish.comments.map(comment => {
                return (
-               <CardBody>
-                 <CardText>{comment.comment}</CardText>
-                 <CardText>-- {comment.author}, {Moment(comment.date).format('MMM d, yyyy')}</CardText>
-               </CardBody>
-               )
-            } );
-        return(
-            <div className="row">
-              <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name}/>
-                    <CardBody>
-                    <CardTitle heading>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-              </div>
-              <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <h4>Comments</h4>
-                    {comments}
-                </Card>
-              </div>
-            </div>
-            )
-            }else {
-                return(
+                    <div className="container">
+                      <div className="row">
+                        {this.renderDish(dish)}
+                        {this.renderComments(dish.comments)}
+                      </div>
+                    </div>
+               );
+            } else {
+                  return (
                     <div></div>
-                )
+                  );
             }
-    }
+          }
 }
 export default DishDetail;
-
