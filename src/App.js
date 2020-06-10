ca8615b05b9ffe650f9c5c7f9cf03519a7580c76
Altaeb <abd2010ly@gmail.com>
2020-06-10 19:24:19 +0200

    Remove logo.svg.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap'
 import Menu from './components/MenuComponent'
 import './App.css';
