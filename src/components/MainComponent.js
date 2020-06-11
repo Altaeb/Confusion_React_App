e1b94197e35053f40db46a7148ac02050f8d61af
Altaeb <abd2010ly@gmail.com>
2020-06-11 16:46:25 +0200

    React Router. we configure our react application to make use of the react route.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import Dishdetail from './DishdetailComponent';
 import Header from './headerComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
+import { Switch, Route, Redirect} from 'react-router-dom'
 
 class Main extends Component {
 
@@ -11,23 +12,25 @@ class Main extends Component {
     super(props);
 
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-}
-
   render(){
+
+    const HomePage = () => {
+      return(
+        <Home />
+      );
+    }
     return (
       <div>
         <Header />
-        <Menu dishes={this.state.dishes} 
-             onClick={(dishId) => this.onDishSelect(dishId)}/>
-        <Dishdetail 
-             dish ={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />     
       </div>
     );
