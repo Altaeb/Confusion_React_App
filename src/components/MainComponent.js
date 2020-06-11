e8e01f8a04cb9d55bd4500f1167c8c66a3bb288a
Altaeb <abd2010ly@gmail.com>
2020-06-11 18:49:53 +0200

    Single Page Applications Part 1. we have already configured our application to use React router, it is very easy to retrofit that into a single page application, rendering in this case, three different components and we are able to navigate to the three different views using the links that we have already built into the header and the footer component.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
-import { Switch, Route, Redirect} from 'react-router-dom'
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import { Switch, Route, Redirect} from 'react-router-dom';
 
 class Main extends Component {
 
@@ -12,7 +16,10 @@ class Main extends Component {
     super(props);
 
     this.state = {
-      dishes: DISHES
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
     };
   }
 
@@ -20,7 +27,9 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home />
+        <Home dish={this.state.dishes.filter((dishes) => dishes.featured)[0]}
+        promotion={this.state.promotions.filter((promotions) => promotions.featured)[0]} 
+        leader={this.state.leaders.filter((leaders) => leaders.featured)[0]}  />
       );
     }
     return (
@@ -29,6 +38,7 @@ class Main extends Component {
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
         <Footer />     
