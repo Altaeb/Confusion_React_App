f33b1b1188ebdf2402ee42250800ada3ae7b7de1
Altaeb <abd2010ly@gmail.com>
2020-06-12 16:41:48 +0200

    Single Page Applications Part 2. in this exercise, we have seen how we can use our parameters to pass information to our React router which enables us to render a dish and so the information about the route parameter can be passed into that dish or rather passed into each component and then used there to appropriately render the view.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -32,12 +33,22 @@ class Main extends Component {
         leader={this.state.leaders.filter((leaders) => leaders.featured)[0]}  />
       );
     }
+
+    const DushWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} 
+        />
+      );
+    }
+
     return (
       <div>
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path="/menu/:dishId" component={DushWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
