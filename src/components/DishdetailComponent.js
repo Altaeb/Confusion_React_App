d968db2194024cfb8a52b81dc38d0a326e568455
Altaeb <abd2010ly@gmail.com>
2020-06-14 21:06:11 +0200

    Assignment  3. Update the DishdetailComponent.js to include a form and do the form validation in code. # Task 1. In this task add a new class component named CommentForm. * Add a new class component named CommentForm that will add a button to the view as shown in the image below. * When the button is clicked, it toggles a modal that will display the comment form. * The CommentForm component is used by the RenderComments function to display the button for toggling the modal. # Task 2. In this task you will construct the form for users to submit their comments as shown in the image below. * Set up the form as a local form using the react-redux-form with the three fields: author, rating and comment. * The rating field in the comment form is implemented using a select, the author is implemented using a text field, while the comment uses a textarea with six rows. # Task 3. In this task, you will enable form validation . * The author field should at least be three characters long. * The author field should be less than or equal to 15 characters. * The user should be alerted by showing the invalid message displayed at the bottom of the field.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,14 @@
-import React from 'react'
-import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import React, { Component } from 'react'
+import { Card, CardImg, CardText, CardBody, CardTitle,
+   Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader,
+    ModalBody, Label, FormGroup, Row} from 'reactstrap';
 import Moment from 'moment';
 import { Link } from 'react-router-dom';
+import { LocalForm, Control, Errors} from 'react-redux-form'
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
 
   function RenderDish({dish}) {
             return(
@@ -41,6 +48,7 @@ import { Link } from 'react-router-dom';
           );
   }
 
+
   const DishDetail = (props) => {
         if (props.dish != null) {
                return (
@@ -58,6 +66,7 @@ import { Link } from 'react-router-dom';
                       <div className="row">
                         <RenderDish dish={props.dish} />
                         <RenderComments comments={props.comments} />
+                        <CommentForm/>
                       </div>
                     </div>
                );
@@ -67,5 +76,82 @@ import { Link } from 'react-router-dom';
                   );
             }
           }
+
+class CommentForm extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      IsModelOpen: false
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+}
+  handleSubmit(values) {
+    this.toggleModal();
+}
+  toggleModal() {
+    this.setState({
+      IsModelOpen: !this.state.IsModelOpen
+    });
+  }
+
+  render() {
+    return(
+        <React.Fragment>
+          <Button outline onClick={this.toggleModal}>
+            <span className="fa fa-pencil fa-lg">Submite Comment</span>
+          </Button>
+          <Modal isOpen={this.state.IsModelOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submite Comment</ModalHeader>
+            <ModalBody>
+              <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                <FormGroup>
+                  <label htmlFor="username">Rating</label>
+                  <Control.select model=".rating" name="rating"
+                  className="form-control">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Control.select>
+                </FormGroup>
+                <FormGroup>
+                  <label htmlFor="firstname">Your Name</label>
+                  <Control.text model=".name" name="name"
+                  className="form-control" placeholder="Your Name"
+                  validators={{
+                    required, minLength: minLength(3), maxLength: maxLength(15)
+                  }}
+                  />
+                  <Errors
+                  className="text-danger"
+                  model=".name"
+                  show="touched"
+                  messages={{
+                    required: 'Required ',
+                    minLength: 'Must be greater than 3 characters',
+                    maxLength: 'Must be 15 characters or less'
+                  }}
+                  />
+                  <FormGroup>
+                    <label htmlFor="comment">Comment</label>
+                    <Control.textarea model=".text" name="text"
+                    className="form-control" rows="6"/>
+                  </FormGroup>
+                </FormGroup>
+                <FormGroup>
+                  <Button outline className="form-control" color="primary">Submit</Button>
+                </FormGroup>
+              </LocalForm>
+            </ModalBody>
+          </Modal>
+        </React.Fragment>
+    );
+  }
+}
 
 export default DishDetail;
