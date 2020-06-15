1e08fe8f5d7b8bf3a41795449e7947cdde3751be
Altaeb <abd2010ly@gmail.com>
2020-06-15 15:20:49 +0200

    React-Redux-Form Revisited. Explore the interaction between react-redux-form and the Redux store, map the form into the store so that the state of the form will be persisted in the store. *  Use react-redux-form to interact with Redux store and store the state of the form in the store.

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Breadcrumb, BreadcrumbItem, Button, Label, Col, Row} from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form'
+import { Control, Form, Errors, Action } from 'react-redux-form'
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -17,8 +17,9 @@ class Contact extends Component {
     }
 
     handleSubmit(values) {
-        console.log("Curent State is: " + JSON.stringify(values))
-        alert("Curent State is: " + JSON.stringify(values))
+        console.log("Curent State is: " + JSON.stringify(values));
+        alert("Curent State is: " + JSON.stringify(values));
+        this.props.resetFeedbackForm();
     }
 
 
@@ -66,7 +67,7 @@ class Contact extends Component {
                         <h3>Send us Your Feedback</h3>
                     </div>
                     <div className="col-12 col-md-9">
-                        <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -190,7 +191,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
