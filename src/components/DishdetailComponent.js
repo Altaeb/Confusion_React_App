612dbed8ef6f98715445bc55c11b02162302ff2b
Altaeb <abd2010ly@gmail.com>
2020-06-16 21:13:30 +0200

    React Animation Components. Use react-animation-components to add more subtle animations. *  Use react-animation-components to add more subtle animations to your React app.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -7,6 +7,7 @@ import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors} from 'react-redux-form';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -14,13 +15,18 @@ const minLength = (len) => (val) => val && (val.length >= len);
   function RenderDish({dish}) {
             return(
               <div className="col-12 col-md-5 m-1">
-                <Card>
-                  <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-                </Card>
+                <FadeTransform in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                  <Card>
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name}/>
+                    <CardBody>
+                      <CardTitle>{dish.name}</CardTitle>
+                      <CardText>{dish.description}</CardText>
+                    </CardBody>
+                  </Card>
+                </FadeTransform>
               </div>
             );
 
@@ -32,14 +38,18 @@ const minLength = (len) => (val) => val && (val.length >= len);
             <div className="col-12 col-md-5 m-1">
               <h4>Comments</h4>
               <ul className="list-unstyled">
+                <Stagger in>
                   {comments.map((comment) => {
                       return (
-                        <li key={comment.id}>
-                          <p>{comment.comment}</p>
-                          <p>-- {comment.author} , {Moment(comment.date).format('MMM d, yyyy')}</p>
-                        </li>
+                        <Fade in>
+                          <li key={comment.id}>
+                            <p>{comment.comment}</p>
+                            <p>-- {comment.author} , {Moment(comment.date).format('MMM d, yyyy')}</p>
+                          </li>
+                        </Fade>
                       );
                   })}
+                </Stagger>
               </ul>
               <CommentForm dishId={dishId} postComment={postComment} />
             </div>
